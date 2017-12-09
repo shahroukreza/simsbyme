bc5d4e7fbc6cab0fa48153d35c34aac6184a6e9d
shahroukreza <34294015+shahroukreza@users.noreply.github.com>
2017-12-09 11:10:45 -0500

    Update sketch.js

--- solarsail/sketch.js
+++ solarsail/sketch.js
@@ -1,54 +1,106 @@
 var BSlider
+var SSlider
 var r
 function setup() {
   createCanvas(windowWidth, windowHeight);
   xii = width/2 + 120;
   xi = xii;
   yi = height/2 ;
-  vi = 0.001;
+  vi = 0;
   v = vi;
   t = 0;
   c=3e8;
   s=1.4e3;
-  BSlider = createSlider(1, 50, 7,0.25);
-  BSlider.position(50,40);
-  MSlider = createSlider(1,50,1,0.25);
-  MSlider.position(50,100);
+  SSlider = createSlider(1,2,1,1);
+  SSlider.position(50,40)
+  BSlider = createSlider(1, 50, 7,0);
+  BSlider.position(50,100);
+  MSlider = createSlider(1,50,1,0);
+  MSlider.position(50,160);
   r = BSlider.value();
+  sh = SSlider.value();
+
 }
+
 
 function draw() {
   background(255)
   fill('yellow')
   stroke('black')
-  r = BSlider.value();
-  ar = 3.14*r^2;
-  m = MSlider.value();
   sun = ellipse(width/2,height/2, 150,150)
   fill('white')
   stroke('black')
-  solsail = ellipse(xi,yi,r,r)
-  a = (s*ar)/(c*m);
-  v = v + 1000*a;
-  xi = xi + v;
-  fill(0)
-  textSize(20)
-  text("Radius: "+ r + " m",50,30)
-  text("Mass: " + m + " kg",50,90)
-  text("Current acceleration : ",100,height/3+60)
-  text(a+" m/s^2",100,height/3+85)
-  text("Current velocity : ", 100,height/3+110)
-  text(v.toFixed(3)+" m/s",100,height/3+135)
-  text("Time to the Moon: ", 100,height/3+160)
-  tmoon = sqrt(2*384.4e6/a)/(60*60*24);
-  text(tmoon.toFixed(2)+" days",100,height/3+185)
-  text("Time to Jupiter: ", 100,height/3+210)
-  tjupiter = sqrt(2*588e9/a)/(60*60*24);
-  text(tjupiter.toFixed(2)+" days",100,height/3+235)
-  if (xi > width)
+  sh = SSlider.value();
+  if (sh == 1)
   {
-    xi = xii;
-    v = vi;
+    r = BSlider.value();
+    solsail = ellipse(xi,yi,r,r);
+    ar = 3.14*r^2;
+    m = MSlider.value();
+    a = (s*ar)/(c*m);
+    v = v + 1000*a;
+    xi = xi + v;
+    fill(0)
+    textSize(20)
+    text("Cross-sectional Shape: Circle",50,30)
+    text("Radius: "+ r.toFixed(2) + " m",50,90)
+    text("Mass: " + m.toFixed(2) + " kg",50,150)
+    text("Current acceleration : ",100,height/3+35)
+    text(a+" m/s^2",100,height/3+60)
+    text("Current velocity : ", 100,height/3+85)
+    text(v.toFixed(3)+" m/s",100,height/3+110)
+    text("Time to the Moon: ", 100,height/3+160)
+    tmoon = sqrt(3.8e8/a)/(60*60*24);
+    text(tmoon.toFixed(2)+" days",100,height/3+185)
+    text("Time to Jupiter: ", 100,height/3+210)
+    tjupiter = sqrt(6.28e11/a)/(60*60*24);
+    text(tjupiter.toFixed(2)+" days",100,height/3+235)
+    text("Time to Neptune: ",100, height/3 + 260)
+    tneptune = sqrt(4.351e12/a)/(60*60*24);
+    text(tneptune.toFixed(2) + " days",100, height/3 + 285)
+    text("Time to the Edge of the Solar System: ",100, height/3 + 310)
+    tedge = sqrt(9e12/a)/(60*60*24);
+    text(tedge.toFixed(2) + " days",100, height/3 + 335)
+    if (xi > width)
+    {
+      xi = xii;
+      v = vi;
+    }
   }
-
-}
+  if (sh==2)
+  {
+    r = BSlider.value();
+    solsail = rect(xi-r,yi-r,r,r);
+    ar = r^2;
+    m = MSlider.value();
+    a = (s*ar)/(c*m);
+    v = v + 1000*a;
+    xi = xi + v;
+    fill(0)
+    textSize(20)
+    text("Cross-sectional Shape: Square",50,30)
+    text("Side length: "+ r.toFixed(2) + " m",50,90)
+    text("Mass: " + m.toFixed(2) + " kg",50,150)
+    text("Current acceleration : ",100,height/3+35)
+    text(a+" m/s^2",100,height/3+60)
+    text("Current velocity : ", 100,height/3+85)
+    text(v.toFixed(3)+" m/s",100,height/3+110)
+    text("Time to the Moon: ", 100,height/3+160)
+    tmoon = sqrt(3.8e8/a)/(60*60*24);
+    text(tmoon.toFixed(2)+" days",100,height/3+185)
+    text("Time to Jupiter (first gas giant): ", 100,height/3+210)
+    tjupiter = sqrt(6.28e11/a)/(60*60*24);
+    text(tjupiter.toFixed(2)+" days",100,height/3+235)
+    text("Time to Neptune (last planet in the solar system): ",100, height/3 + 260)
+    tneptune = sqrt(4.351e12/a)/(60*60*24);
+    text(tneptune.toFixed(2) + " days",100, height/3 + 285)
+    text("Time to the Edge of the Solar System: ",100, height/3 + 310)
+    tedge = sqrt(9e12/a)/(60*60*24);
+    text(tedge.toFixed(2) + " days",100, height/3 + 335)
+    if (xi > width)
+    {
+      xi = xii;
+      v = vi;
+    }
+  }
+  }
